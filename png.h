c20b2413eea86802a26ef6c9526902c6924a8803
Matthew Chase Whittemore <mcwhittemore@gmail.com>
2017-07-12 23:23:35 -0400

    some docs

--- png.h
+++ png.h
@@ -5,9 +5,23 @@
 
 namespace pixicog {
   namespace png {
+/**
+ * Opens a local file png and fills an image
+ * @name open
+ * @public
+ * @param imgPtr {&Image} - the image to be filled
+ * @param filepath {std-string} - the file to be opened
+ */
     void open(pixicog::Image *imgPtr, std::string filepath) {
       throw "not implemented yet";
     }
+/**
+ * Saves a pixicog::Image to a local file
+ * @name save
+ * @public
+ * @param imgPtr {&Image} - the image to be saved
+ * @param filepath {std-string} - the file to write to
+ */
     void save(pixicog::Image *imgPtr, std::string filepath) {
       throw "not implemented yet";
     }
